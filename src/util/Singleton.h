36100e8fa9ac75269c9457dd049bcbd4b06c0e5f
HarryZhou <harry@DESKTOP-LGCTLOU>
2025-02-06 00:39:34 +0800

    Deprecated argtypes in single ton which is wrong

--- src/util/Singleton.h
+++ src/util/Singleton.h
@@ -7,17 +7,17 @@
 
 namespace sylar {
     
-template<class T, class... ArgTypes>
+template<class T>
 class Singleton {
 private:
     Singleton() = default;
     Singleton(const Singleton&) = delete;
     Singleton& operator=(const Singleton&) = delete;
 public:
-    static T* getInstance(ArgTypes ...args) {
+    static T* getInstance() {
         std::lock_guard<std::mutex> lock(m_mtx);
         if(m_instance == nullptr) {
-            m_instance = new T(args...);
+            m_instance = new T();
         }
         return m_instance;
     }
@@ -26,11 +26,11 @@ private:
     static std::mutex m_mtx;
 };
 
-template<class T, class... ArgTypes>
-T* Singleton<T, ArgTypes...>::m_instance = nullptr;
+template<class T>
+T* Singleton<T>::m_instance = nullptr;
 
-template<class T, class... ArgTypes>
-std::mutex Singleton<T, ArgTypes...>::m_mtx;
+template<class T>
+std::mutex Singleton<T>::m_mtx;
 
 template<class T>
 class SingletonPtr {
@@ -50,6 +50,12 @@ private:
     static std::shared_ptr<T> m_instance;
     static std::mutex m_mtx;
 };
+
+template<class T>
+std::shared_ptr<T> SingletonPtr<T>::m_instance = nullptr;
+
+template<class T>
+std::mutex SingletonPtr<T>::m_mtx;
 
 } // namespace sylar
 
